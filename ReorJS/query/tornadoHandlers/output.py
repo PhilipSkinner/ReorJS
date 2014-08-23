92157cb3fbd191f07284ef8c655342ae21a73ea9
Philip Skinner <philip@crowdca.lc>
2014-08-23 11:08:25 +0200

    time for a break

---
+++ ReorJS/query/tornadoHandlers/output.py
@@ -0,0 +1,13 @@
+from base import *
+
+class GetTask(BaseHandler):
+  def get(self):
+    print "nothing"
+
+class Ping(BaseHandler):
+  def get(self):
+    print "nothing"
+    
+class Status(BaseHandler):
+  def get(self):
+    print "nothing"
