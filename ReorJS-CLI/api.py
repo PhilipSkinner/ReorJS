0f8a2dc88ec9ed9da37eeb70d980644165b0211e
Philip Skinner <philip@crowdca.lc>
2014-08-24 10:11:11 +0200

    added licensing to cli

--- ReorJS-CLI/api.py
+++ ReorJS-CLI/api.py
@@ -1,3 +1,31 @@
+"""
+	api.py
+	Simple ReorJS API connector
+	
+	--
+	Provides simple programmatic access to the ReorJS server API calls for managing
+	ReorJS application, dataset and task objects
+	--
+	
+	Author(s) 	- Philip Skinner (philip@crowdca.lc)
+	Last modified 	- 2014-08-24
+	
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	    
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	         
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	
+	Copyright (c) 2014, Crowdcalc B.V.
+"""
+
 import urllib2
 import httplib2
 import simplejson as json
