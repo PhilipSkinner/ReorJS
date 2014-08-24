eda8e83d0581c2d09b3d7826e5b8224cf130992e
Philip Skinner <philip@crowdca.lc>
2014-08-24 09:28:34 +0200

    added some help text and some missing cmd stuff in the cli

--- ReorJS-CLI/main.py
+++ ReorJS-CLI/main.py
@@ -16,7 +16,7 @@ class ReorJSCLI(cmd.Cmd):
   api = api.API()
   
   prompt = bcolors.HEADER + '[reorjs]: ' + bcolors.ENDC
-  intro = bcolors.OKBLUE + 'Welcome to ReorJS!' + bcolors.ENDC + '\n\nTo initialize session please connect to reorjsd (see help connect)\n'
+  intro = bcolors.OKBLUE + '\nWelcome to ReorJS!' + bcolors.ENDC + '\n\nTo initialize session please connect to reorjsd (see help connect)\n'
   
   ##
   # connect: allows user to specify API details for client
@@ -75,14 +75,48 @@ class ReorJSCLI(cmd.Cmd):
       
       return
     elif details[0] == 'create':
-      self.api.createTask()
-    elif details[0] == 'delete':
-      self.api.deleteTask()
+      if len(details) < 3:
+        return self.help_task(task=details[0])
+      
+      application = details[1]
+      dataset = details[2]
+      
+      status = self.api.createTask(application=application, dataset=dataset)      
+      self.doStatus(status)
+      
+      return
+    elif details[0] == 'help' and len(details) == 2:
+      return self.help_task(task=details[0], specific=details[1])
       
     return self.help_task()
     
-  def help_task(self, task=None):
-    self.bad("Usage: task ")
+  def help_task(self, task=None, specific=None, name=None):
+    if specific == None:
+      specific = task
+      task = 'help'
+    
+    if task == 'help':
+      if specific == 'list':
+        self.bad('Usage: task list')
+        self.normal('Lists all of the current tasks')
+        return
+      elif specific == 'detail':
+        self.bad('Usage: task detail [id]')
+        self.normal('Shows the specifics for task [i]')
+        return
+      elif specific == 'create':
+        self.bad('Usage: task create [application id] [dataset id]')
+        self.normal('Creates a task by combining application [application id] and dataset [dataset id]')
+        return
+
+    #if we got this far we need to show the default
+    self.header('\nTasks')
+    self.header('------------')
+    self.normal('Tasks map an application to a set of data and starts the computation on the connected ReorJS Javascript clients.\n')
+    self.bad('Usage: task [command] [options...]')    
+    self.info('Commands: list detail create\n')
+    self.normal('See task help [command] for more information\n')
+    return
   
   ##
   # task: end
@@ -114,16 +148,118 @@ class ReorJSCLI(cmd.Cmd):
       
       return
     elif details[0] == 'create':
-      self.api.createDataset()
+      #we need a minimum of 2 values
+      if len(details) < 3:
+        return self.help_dataset(task=details[0])
+      
+      #fill up the other entries
+      while len(details) < 9:
+        details.append(None)
+                
+      name 		= details[1]
+      source_type 	= str(details[2]).lowercase
+      source_hostname 	= details[3]
+      source_port 	= details[4]
+      source_name 	= details[5]
+      source_table 	= details[6]
+      source_username 	= details[7]
+      source_password 	= details[8]
+    
+      #check our type to be a supported type
+      if source_type not in ['mysql', 'redis', 'mongo', 'hadoop', 'memcached', 'local']:
+        return self.help_dataset(task=details[0], specific='type', name=source_type)
+    
+      status = self.api.createDataset(name		= name,
+                                      source_type 	= source_type,
+                                      source_hostname 	= source_hostname,
+                                      source_port 	= source_port,
+                                      source_name 	= source_name,
+                                      source_table 	= source_table,
+                                      source_username 	= source_username,
+                                      source_password 	= source_password)
+      self.doStatus(status)
+      return      
     elif details[0] == 'modify':
-      self.api.modifyDataset()
+      #need atleast id, name and type
+      if len(details) < 4:
+        return self.help_dataset(task=details[0])
+
+      #fill up the other entries
+      while len(details) < 10:
+        details.append(None)
+        
+      id 		= details[1]
+      name		= details[2]
+      source_type	= details[3]
+      source_hostname	= details[4]
+      source_port	= details[5]
+      source_name	= details[6]
+      source_table	= details[7]
+      source_username	= details[8]
+      source_password	= details[9]
+      
+      #check our type to be a supported type
+      if source_type not in ['mysql', 'redis', 'mongo', 'hadoop', 'memcached', 'local']:
+        return self.help_dataset(task=details[0], specific='type', name=source_type)
+      
+      status = self.api.modifyDataset(name		= name,
+                                      source_type	= source_type,
+                                      source_hostname	= source_hostname,
+                                      source_port	= source_port,
+                                      source_name	= source_name,
+                                      source_table	= source_table,
+                                      source_username	= source_username,
+                                      source_password	= source_password)
+      self.doStatus(status)
+      return
     elif details[0] == 'delete':
-      self.api.deleteDataset()
+      if len(details) < 2:
+        return self.help_dataset(task=details[0])
+
+      status = self.api.deleteDataset(id=details[1])
+      self.doStatus(status)
+            
+      return
+    elif details[0] == 'help' and len(details) == 2:
+      return self.help_dataset(task=details[0], specific=details[1])
     
     return self.help_dataset()
     
-  def help_dataset(self, task=None):
-    self.bad('Usage: dataset ')
+  def help_dataset(self, task=None, specific=None, name=None):
+    if specific == None:
+      specific = task
+      task = 'help'
+    
+    if task == 'help':
+      if specific == 'list':
+        self.bad('Usage: dataset list')
+        self.normal('List all datasets currently available')      
+        return
+      elif specific == 'detail':
+        self.bad('Usage: dataset detail [id]')
+        self.normal('Shows the specific details for the dataset referenced by id [id]')
+        return
+      elif specific == 'create':
+        self.bad('Usage: dataset create [name] [type] *hostname* *port* *dbname* *tablename* *username* *password*')
+        self.normal('Creates a dataset of [type] using the given options. Please make sure to pass all options that are required for your dataset. Skip any unrequired options by entering a single 1')
+        return
+      elif specific == 'modify':
+        self.bad('Usage: dataset modify [id] [name] [type] *hostname* *port* *dbname* *tablename* *username* *password*')
+        self.normal('Modifies the dataset referenced by [id] using the given options. Please make sure to pass all options that are required for your dataset. Skip any unrequired options by entering a single 1')
+        return
+      elif specific == 'delete':
+        self.bad('Usage: dataset delete [id]')
+        self.normal('Removes the dataset [id]')
+        return
+
+    #if we got this far we need to show the default
+    self.header('\nDatasets')
+    self.header('------------')
+    self.normal('Datasets are sources of information. Currently supported sources are mysql, mongo, redis, memcached, hadoop and the local API\n')
+    self.bad('Usage: dataset [command] [options...]')    
+    self.info('Commands: list detail create modify delete\n')
+    self.normal('See dataset help [command] for more information\n')
+    return
   
   ##
   # dataset: end
@@ -155,20 +291,75 @@ class ReorJSCLI(cmd.Cmd):
       
       return
     elif details[0] == 'create':
-      self.api.createApplication()
+      if len(details) < 3:
+        return self.help_application(task=details[0])
+      
+      name 	= details[1]
+      program 	= details[2]
+      
+      status = self.api.createApplication(name = name, program = program)
+      self.doStatus(status)
       return
     elif details[0] == 'modify':
-      self.api.modifyApplication()
+      if len(details) < 4:
+        return self.help_application(task=details[0])
+      
+      id 	= details[1]
+      name	= details[2]
+      program	= details[3]
+      
+      status = self.api.modifyApplication(id = id, name = name, program = program)
+      self.doStatus(status)
       return
     elif details[0] == 'delete':
-      self.api.deleteApplication()
+      if len(details) < 2:
+        return self.help_application(task=details[0])
+      
+      status = self.api.deleteApplication(id=details[0])
+      self.doStatus(status)
       return
+    elif details[0] == 'help' and len(details) == 2:
+      return self.help_application(task=details[0], specific=details[1])
     
     return self.help_application(task=task)
   
-  def help_application(self, task=None):
-    self.bad("Application help")
-  
+  def help_application(self, task=None, specific=None, name=None):
+    #convert stuff
+    if specific == None:
+      specific = task
+      task = 'help'
+      
+    if task == 'help':
+      if specific == 'list':
+        self.bad('Usage: application list')
+        self.normal('List all applications currently available')
+        return
+      elif specific == 'detail':
+        self.bad('Usage: application detail [application id]')
+        self.normal('Shows the specific details for an application by id')
+        return
+      elif specific == 'create':
+        self.bad('Usage: application create [name] [program]')
+        self.normal('Creates an application named [name] with the Javascript program [program]')
+        return
+      elif specific == 'modify':
+        self.bad('Usage: application modify [id] [name] [program]')
+        self.normal('Updates the application referenced by [id] to have name [name] and Javascript program [program]')
+        return
+      elif specific == 'delete':
+        self.bad('Usage: application delete [id]')
+        self.normal('Removes application [id]')
+        return
+
+    #if we got this far we need to show the default
+    self.header('\nApplications')
+    self.header('------------')
+    self.normal('Applications are javascript functions that can be run against data within the system.\n')
+    self.bad('Usage: application [command] [options...]')    
+    self.info('Commands: list detail create modify delete\n')
+    self.normal('See application help [command] for more information\n')
+    return
+      
   ##
   # application: end
   ##
@@ -200,9 +391,15 @@ class ReorJSCLI(cmd.Cmd):
       table.add_row(rec.values())
       
     print table    
+    
+  def doStatus(self, status):
+    print status
   
   def normal(self, line):
     print line
+  
+  def header(self, line):
+    print bcolors.HEADER + line + bcolors.ENDC
   
   def bad(self, line):
     print bcolors.WARNING + line + bcolors.ENDC
